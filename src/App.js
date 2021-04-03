a237e79c18f6eff589d96cea00f70fe1c5c851b3
zonghuang <1683766027@qq.com>
2021-04-03 08:37:08 +0800

    引入redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { memo } from "react";
+import { Provider } from 'react-redux';
 import { renderRoutes } from "react-router-config";
 import { HashRouter } from "react-router-dom";
 
 import routes from "./router";
+import store from './store';
 
 import AppHeader from "@/components/app-header";
 import AppFooter from "@/components/app-footer";
 
 export default memo(function App() {
   return (
-    <HashRouter>
+    <Provider store={store}>
+      <HashRouter>
       <AppHeader />
       {renderRoutes(routes)}
       <AppFooter />
     </HashRouter>
+    </Provider>
   );
 });
--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,8 +1,9 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { renderRoutes } from 'react-router-config';
 import { NavLink } from 'react-router-dom';
 
 import { dicoverMenu } from "@/common/local-data";
+import request from "@/services/request";
 
 import {
   DiscoverWrapper,
@@ -10,6 +11,15 @@ import {
 } from './style';
 
 export default memo(function Discover(props) {
+
+  useEffect(() => {
+    request({
+      url: "/banner"
+    }).then(res => {
+      console.log(res);
+    })
+  }, [])
+
   const { route } = props;
 
   return (
